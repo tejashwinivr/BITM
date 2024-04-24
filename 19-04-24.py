aea62113941f34e7f7a03eecc598ec627a35deb5
tejashwinivr <167219541+tejashwinivr@users.noreply.github.com>
2024-04-24 09:27:09 +0530

    Add files via upload

---
+++ 19-04-24.py
@@ -0,0 +1,19 @@
+'''def teju():
+    return "this is my account"
+test_dict={"fname":teju,"age":20}
+print("the original dictionary "+str(test_dict))
+res=test_dict['fname']()
+print("the required call result "+str(res))#based on return value the return type can be changed
+
+def teju(a,b):
+    print("account balance:",(a+b))
+test_dict={"fname":teju,"age":20}
+print("the original dictionary "+str(test_dict))
+test_dict['fname'](10,10)
+
+#swapping of two numbers
+a,b=10,20
+a,b=b,a
+print(a,b)'''
+
+
