4e8d29b211fb00cdfec5b2d36438dde75d3bba96
tejashwinivr <167219541+tejashwinivr@users.noreply.github.com>
2024-04-27 11:38:29 +0530

    Add files via upload

---
+++ hackerrank.py
@@ -0,0 +1,13 @@
+'''
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+print(a//b)
+print(a/b)'''
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+print(a+b)
+print(a-b)
+print(a*b)
